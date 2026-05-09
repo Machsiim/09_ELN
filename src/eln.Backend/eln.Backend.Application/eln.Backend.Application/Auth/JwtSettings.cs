dffbf6b86442c876f9a350293fbf60e21ff572e8
Machsiim <machsimbs@gmail.com>
2026-05-09 21:43:37 +0200

    increased token expiration

--- src/eln.Backend/eln.Backend.Application/eln.Backend.Application/Auth/JwtSettings.cs
+++ src/eln.Backend/eln.Backend.Application/eln.Backend.Application/Auth/JwtSettings.cs
@@ -11,6 +11,6 @@ namespace eln.Backend.Application.Auth
         public string Secret { get; set; } = "";
         public string Issuer { get; set; } = "";
         public string Audience { get; set; } = "";
-        public int ExpirationInMinutes { get; set; } = 60;
+        public int ExpirationInMinutes { get; set; } = 480;
     }
 }
